431780799e5aa1ba4411d7dbe914befbc031dbb9
Jeremy Harper <hunter@ladyhunterbear.ca>
2021-10-03 07:57:43 -0700

    Update Hazard value
    Move hazard value down to prefer a number of other moves over moving into the Hazard squares.
    Refactor methods to decrease verbosity and improve clarity.

--- battlesnake2/game.py
+++ battlesnake2/game.py
@@ -33,37 +33,32 @@ class Game:
     
     
     def setup_gameboard(self, request: object):
-        # create a request reader to parse the json request object
         request = RequestReader(request)
-        # create a state object to store game properties
         self.state = GameState()
-        # create the game board
         self.state.set_gameboard(request.gameboard())
-        # add my snake
         self.state.set_my_snake(request.my_snake())
         self.add_my_snake_squares()
-        # add opponents
         self.state.set_opponents(request.opponents())
         if self.state.get_opponents().count() > 0:
             self.add_opponent_snake_squares()
-        # add food
         self.state.set_food(request.food())
         self.add_to_gameboard(self.state.get_food())
-        # add hazards
         self.state.set_hazards(request.hazards())
         self.add_to_gameboard(self.state.get_hazards())
-        # specify ruleset
         self.state.set_ruleset(request.ruleset())
-        
+
+
     def add_opponent_snake_squares(self):
         opponents = self.state.get_opponents()
         for id in opponents.get_ids():
             snake_squares = SnakeSquares(self.state.get_my_snake(), opponents.get(id))
             self.add_to_gameboard(snake_squares.get_squares())
+
         
     def add_my_snake_squares(self):
         snake_squares = SnakeSquares(self.state.get_my_snake())
         self.add_to_gameboard(snake_squares.get_squares())
+
 
     def add_to_gameboard(self, square_collection: GameBoardSquareCollection):
         gameboard = self.state.get_gameboard()
@@ -71,35 +66,18 @@ class Game:
         for key in squares:
             gameboard.set_square(squares[key])
         self.state.set_gameboard(gameboard)
+
          
     def set_strategy(self) -> GameStrategy:
-        game_strategy_factory = GameStrategyFactory()
-        self.strategy = game_strategy_factory.make(self.state)
-    
-    # def get_next_move_score(self, coord: Coordinate) -> float:
-    #     max_move_score = 300 # total of highest possible scoring moves (food)
-    #     next_move_score = 0
-    #     possible_next_moves = self.gameboard.get_possible_directions(coord)
-    #     for direction in possible_next_moves:
-    #         next_move_score = next_move_score + self.gameboard.get_square(possible_next_moves[direction]).get_state_value()
-    #     return next_move_score / max_move_score
-    #     
-    #     
+        self.strategy = GameStrategyFactory().make(self.state)
+
 
     def relative_direction(self, origin: Coordinate, destination: Coordinate) -> str:
-        direction = ''
-        if destination.get_y() == origin.get_y() and destination.get_x() > origin.get_x():
-            direction = 'right'
-        elif destination.get_y() == origin.get_y() and destination.get_x() < origin.get_x():
-            direction = 'left'
-        elif destination.get_y() > origin.get_y() and destination.get_x() == origin.get_x():
-            direction = 'up'
-        elif destination.get_y() < origin.get_y() and destination.get_x() == origin.get_x():
-            direction = 'down'
-        else:
-            direction = 'invalid'
-        return direction
-        
+        if destination.get_y() != origin.get_y():
+            return 'up' if destination.get_y() > origin.get_y() else 'down'
+        return 'right' if destination.get_x() > origin.get_x() else 'left'
+
+
     '''
     Evaluate the game board
     '''
@@ -132,6 +110,6 @@ class Game:
     def get_move(self):
         choice = self.evaluate_gameboard()
         board_matrix = self.state.get_gameboard().get_board_matrix()
-        print(board_matrix)
-        print(choice)        
+        # print(board_matrix)
+        # print(choice)
         return {"move": choice, "shout": str("I should move " + choice)}
